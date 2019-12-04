87967413cbf117e10a237fc852ec8b2e2cd73998
Takayuki Uchida <uchida190501@lhl.co.jp>
2019-12-04 10:43:05 +0900

    init

---
+++ init.el
@@ -0,0 +1,82 @@
+;; Set the environment to Japanese, UTF-8
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems' utf-8)
+(prefer-coding-system 'utf-8)
+
+;; Do not display startup message
+(setq inhibit-startup-message t)
+
+;; Do not make backup file
+(setq make-backup-files nil)
+
+;; Delete auto save file when finished
+(setq delete-auto-save-files t)
+
+;; use spaces in tabs
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; Display line feed code
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
+
+;; make window transparent
+;; Active window / Inactive window (alpha value specifies transparency)
+(add-to-list 'default-frame-alist' (alpha. (0.85 0.85)))
+
+;; Remove the menu bar
+(menu-bar-mode -1)
+
+;; Remove toolbar
+(tool-bar-mode -1)
+
+;; display the number of columns
+(column-number-mode t)
+
+;; show number of lines
+(global-linum-mode t)
+
+;; Stop blinking the cursor
+(blink-cursor-mode 0)
+
+;; Highlight cursor line
+(global-hl-line-mode t)
+
+;; flash the corresponding parenthesis
+(show-paren-mode 1)
+
+;; Visualize spaces, tabs, etc.
+(global-whitespace-mode 1)
+
+;; scroll line by line
+(setq scroll-conservatively 1)
+
+;; straight.el setting
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; evil.el setting
+(straight-use-package 'evil)
+(evil-mode 1)
+
+;; color theme setting
+(straight-use-package 'nord-theme)
+(load-theme 'nord t)
+
+;; all-the-icons.el setting
+(straight-use-package 'all-the-icons)
